9da53bee29b1301de06812cd5db39c07cdc6240d
nazmul56 <nazmul_104396@yahoo.com>
2015-08-24 15:24:57 +0600

    This tutorial shows how to connect Java class to xml layout file.

--- app/src/main/java/practice/nazmul/practice/StartingPoint.java
+++ app/src/main/java/practice/nazmul/practice/StartingPoint.java
@@ -4,14 +4,45 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class StartingPoint extends ActionBarActivity {
+    int counter;
+    Button add, sub;
+    TextView display;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_starting_point);
+
+        counter = 0;
+        add = (Button)findViewById(R.id.bAdd);
+        sub =(Button)findViewById(R.id.bSub);
+        display =(TextView)findViewById(R.id.tvdisplay);
+
+        add.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+             counter++;
+                display.setText("Your Total is : " + counter);
+
+            }
+
+
+        });
+        sub.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                counter--;
+                display.setText("Your Total is : " + counter);
+            }
+
+
+        });
+
+
     }
 
     @Override
