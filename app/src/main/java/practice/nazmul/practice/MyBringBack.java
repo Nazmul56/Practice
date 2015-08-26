e38ca8b9484d86af1d9b5ca802a92c15a35ef40c
nazmul56 <nazmul_104396@yahoo.com>
2015-08-26 16:39:32 +0600

    Animate the Green Ball

--- app/src/main/java/practice/nazmul/practice/MyBringBack.java
+++ app/src/main/java/practice/nazmul/practice/MyBringBack.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.view.View;
 
 /**
@@ -12,6 +15,7 @@ import android.view.View;
 public class MyBringBack extends View {
 
         Bitmap gBall;
+        float changingY =0;
     public MyBringBack(Context context) {   // Constructor
         super(context);
         //TODO Auto-generated constructor
@@ -25,8 +29,20 @@ public class MyBringBack extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         canvas.drawColor(android.R.color.white);//
-        canvas.drawBitmap(gBall,(canvas.getWidth()/2), 0,null);// set the position of image
+        canvas.drawBitmap(gBall, (canvas.getWidth() / 2), changingY, null);// set the position of image
+        if(changingY < canvas.getHeight()){
+            changingY += 10; //It will increment 10 pixel
+
+        }else {
+            changingY = 0;
+        }
 
 
+        Rect middleRect = new Rect();
+        middleRect.set(0, 400, canvas.getWidth(), 550);
+        Paint ourBlue = new Paint();
+        ourBlue.setColor(Color.BLUE);
+        canvas.drawRect(middleRect,ourBlue);
+        invalidate();
     }
 }
